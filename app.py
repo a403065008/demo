828a099474232fed66bf7e221823701936023de9
a403065008 <a403065008@gmail.com>
2021-11-19 17:09:36 +0800

    test

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+from flask.helpers import url_for
+from werkzeug.utils import redirect
+
+dog = Flask(__name__)
+
+
+@dog.route('/')
+def main():
+    return render_template('index.html')
+@dog.route('/home')
+def hello():
+    return render_template('test.html')
+
+if __name__=='__main__':
+    dog.run(host="0.0.0.0")
+    
